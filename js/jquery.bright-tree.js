d4d797db3d17966c8aff161216874a871862e828
Desharnais Mikael <mikael.desharnais@gmail.com>
2014-08-29 00:16:23 +0200

    Features added : - select manager : allow the customization of the select manager (how many elements selected, how they are deselected) - init select manager : allow the customization of the init select manager (how the elements are selected on init) - init status manager : allow the customization of the init status manager (the status Open/Close on init) - label manager : allow the customization of the label manager (the label for each tree element) - tree manager : allow the customization of the tree manager (how to find the children elements for each tree element) - set root elements : can add an element to the tree - redraw : allow the tree to be redrawn - elements are now unselectable/selectable - walk : allow to walk all the elements of a tree

--- js/jquery.bright-tree.js
+++ js/jquery.bright-tree.js
@@ -1,4 +1,5 @@
 (function ($) {
+	$.BrightTree = BrightTree;
     $.fn.brightTree = function (method) {
 		var argumentsToPass = Array.prototype.slice.call(arguments);
 		if (typeof method == "object"){
@@ -10,79 +11,61 @@
 		* Attach to each DOM element a BrightTree Object and call the init method if there is no object attached
 		* if the object already exists, call the given method with the given parameters
 		*/
-        return this.each(function (key,element) {
+		var toReturn;
+        this.each(function (key,element) {
 			if (typeof jQuery(element).data('bright-tree') == "undefined"){
 				jQuery(element).data('bright-tree', new BrightTree(jQuery(element)));
 				method = "init";
 			}
 			jQuery(element).data('bright-tree')[method].apply(jQuery(element).data('bright-tree'),argumentsToPass);
+			toReturn = jQuery(element).data('bright-tree');
 		});
+		if (this.length==1){
+			return toReturn;
+		}
     };
 
-	function BrightTreeManager(element){
-		return element.getData().children;		
-	}
-	function BrightLabelManager(element){
-		return element.getData().labelOfPipo+" - "+(new Date());
-	}
-	function BrightSelectManager(){
-		this.selectedElement;
-		this.elementSelectedListener = [];
-		this.elementUnselectedListener = [];
-		this.select = function(element){
-			if (this.selectedElement==element){
-				return;
-			}
-			if (typeof this.selectedElement != "undefined"){
-				this.selectedElement.unselect();
-				this.propagateElementUnselected(this.selectedElement);
-			}
-			element.select();
-			this.selectedElement = element;
-			this.propagateElementSelected(element);
-		}
-		this.propagateElementSelected = function(element){
-			for(var i in this.elementSelectedListener){
-				this.elementSelectedListener[i](element);
-			}
-		}
-		this.propagateElementUnselected = function(element){
-			for(var i in this.elementUnselectedListener){
-				this.elementUnselectedListener[i](element);
-			}
-		}
-		this.addElementSelectedListener = function(listener){
-			this.elementSelectedListener.push(listener);
-		}
-		this.addElementUnselectedListener = function(listener){
-			this.elementUnselectedListener.push(listener);
-		}
-	}
 	function BrightTreeElement(tree,data){
 		this.tree = tree;
 		this.htmlElement = jQuery('<li></li>');
+		this.labelContainer = jQuery('<div></div>');
 		this.actionner = jQuery('<a href="#"><i class="icon-ok-sign"></i></a>');
-		this.label = jQuery('<a href="#"></a>');
+		this.label = jQuery('<a href="#" class="bright-tree-label"></a>');
 		this.childrenContainer = jQuery('<ul style="display : none;"></ul>');
 		this.data = data;
 		this.children = [];
 		this.isOpen = false;
 		this.isSelected = false;
+		this.isSelectable = true;
 		
-		this.htmlElement.append(this.actionner);
-		this.htmlElement.append(this.label);
+		this.htmlElement.append(this.labelContainer);
+		this.labelContainer.append(this.actionner);
+		this.labelContainer.append(this.label);
 		this.htmlElement.append(this.childrenContainer);
 		
 		var parent = this;
-		
+		this.getTree = function(){
+			return this.tree;
+		};
 		this.actionner.click(function(event){
 			event.preventDefault();
 			parent.toogleStatus();
 		});
 		this.label.click(function(event){
 			event.preventDefault();
+			if (!parent.isSelectable){
+				return;
+			}
 			parent.tree.selectElement(parent);
 		});
+		this.setSelectable = function(isSelectable){
+			this.isSelectable = isSelectable;
+			if (this.isSelectable){
+				this.htmlElement.removeClass('unselectable');
+			}else {
+				this.htmlElement.addClass('unselectable');
+			}
+		}
 		this.select = function(){
 			this.isSelected = true;
 			this.htmlElement.addClass('selected');
@@ -136,19 +119,31 @@
 			}
 			this.setStatus(this.isOpen);
 		}
+		this.walk = function(walkFunction){
+			for(var i in this.children){
+				walkFunction(this.children[i]);
+				this.children[i].walk(walkFunction);
+			}
+		};
 	}
 	function BrightTree(htmlContainer){
 		var defaultSettings = $.extend($.fn.brightTree.settings,{
-				"tree-manager" : BrightTreeManager,
-				"label-manager" : BrightLabelManager,
-				"select-manager" : new BrightSelectManager(),
+				"tree-manager" : BrightTree.treeManagers.simple,
+				"label-manager" : BrightTree.labelManagers.simple,
+				"select-manager" : new BrightTree.selectManagers.simple(),
+				"init-select-manager" : BrightTree.initSelectManagers.none,
+				"init-status-manager" : BrightTree.initStatusManagers.allClosed,
 				"root-elements" : []
 		    });
+		this.children = [];
 		this.selectElement = function(element){
 			this.getSelectManager().select(element);
 		}
 		this.getSelectManager = function(){
 			return this.settings['select-manager'];
+		}
+		this.getRootElements = function(){
+			return this.settings['root-elements'];
 		}
 		this.init = function(options){
 			this.settings = $.extend(defaultSettings);
@@ -157,28 +152,153 @@
 					this.settings[i] = options[i];
 				}
 			}
-			var rootHtmlElement = jQuery('<ul></ul>');
+			this.redraw();
+		}
+		this.redraw = function(){
+			var rootHtmlElement = jQuery('<ul class="bright-tree"></ul>');
+			htmlContainer.empty();
+			this.children.length = 0;
 			htmlContainer.append(rootHtmlElement);
 			for(var i in this.settings['root-elements']){
-				var treeElement = new BrightTreeElement(this,this.settings['root-elements'][i]);
+				var treeElement = this.createTreeElementFromData(this.settings['root-elements'][i]);
+				if (this.settings['init-select-manager'](treeElement)){
+					this.selectElement(treeElement);
+				}
 				this.appendChild(rootHtmlElement,treeElement);
+				if (this.settings['init-status-manager'](treeElement)){
+					treeElement.setStatus(true);
+				}
+				this.children.push(treeElement);
 			}
-			
+		}
+		this.createTreeElementFromData = function(data){
+			return new BrightTreeElement(this,data);
 		}
 		this.appendChild = function(container,treeElement){
 			container.append(treeElement.getHtmlElement());
 			var children = this.settings['tree-manager'](treeElement);
 			if (typeof children != "undefined" && children.length>0){
 				for(var i = 0; i < children.length;i++){
-					var childrenTreeElement = new BrightTreeElement(this,children[i]);
+					var childrenTreeElement = this.createTreeElementFromData(children[i]);
+					if (this.settings['init-select-manager'](childrenTreeElement)){
+						this.selectElement(childrenTreeElement);
+					}
 					treeElement.appendChild(childrenTreeElement);
+					if (this.settings['init-status-manager'](childrenTreeElement)){
+						childrenTreeElement.setStatus(true);
+					}
 				}
 			}
 		}
 		this.getHtmlContainer = function(){
 			return htmlContainer;
 		};
+		this.walk = function(walkFunction){
+			for(var i in this.children){
+				walkFunction(this.children[i]);
+				this.children[i].walk(walkFunction);
+			}
+		};
 	}
+	BrightTree.treeManagers = {
+		simple : function(element){
+			return element.getData().children;		
+		}
+	};
+	
+	BrightTree.labelManagers = {
+		simple : function(element){
+			return element.getData().name;
+		}
+	};
+	BrightTree.initSelectManagers = {
+		none : function (element){
+			return false;
+		},
+		all : function (element){
+			return true;
+		}
+	};
+	BrightTree.initStatusManagers = {
+		allClosed : function(element){
+			return false;
+		},
+		allOpened : function(element){
+			return true;
+		}
+	};
+	BrightTree.selectManagers = { simple : function (){
+			this.selectedElement;
+			this.elementSelectedListener = [];
+			this.elementUnselectedListener = [];
+			this.select = function(element){
+				if (this.selectedElement==element){
+					return;
+				}
+				if (typeof this.selectedElement != "undefined"){
+					this.selectedElement.unselect();
+					this.propagateElementUnselected(this.selectedElement);
+				}
+				element.select();
+				this.selectedElement = element;
+				this.propagateElementSelected(element);
+			}
+			this.getSelectedElement = function(){
+				return this.selectedElement;
+			}
+			this.propagateElementSelected = function(element){
+				for(var i in this.elementSelectedListener){
+					this.elementSelectedListener[i](element);
+				}
+			}
+			this.propagateElementUnselected = function(element){
+				for(var i in this.elementUnselectedListener){
+					this.elementUnselectedListener[i](element);
+				}
+			}
+			this.addElementSelectedListener = function(listener){
+				this.elementSelectedListener.push(listener);
+			}
+			this.addElementUnselectedListener = function(listener){
+				this.elementUnselectedListener.push(listener);
+			}
+		},
+		multiple : function(){
+			this.selectedElementList = [];
+			this.elementSelectedListener = [];
+			this.elementUnselectedListener = [];
+			this.select = function(element){
+				if (this.selectedElementList.indexOf(element)>=0){
+					this.selectedElementList.splice(this.selectedElementList.indexOf(element),1);
+					element.unselect();
+					this.propagateElementUnselected(element);
+					return;
+				}
+				element.select();
+				this.selectedElementList.push(element);
+				this.propagateElementSelected(element);
+			}
+			this.getSelectedElementList = function(){
+				return this.selectedElementList;
+			}
+			this.propagateElementSelected = function(element){
+				for(var i in this.elementSelectedListener){
+					this.elementSelectedListener[i](element);
+				}
+			}
+			this.propagateElementUnselected = function(element){
+				for(var i in this.elementUnselectedListener){
+					this.elementUnselectedListener[i](element);
+				}
+			}
+			this.addElementSelectedListener = function(listener){
+				this.elementSelectedListener.push(listener);
+			}
+			this.addElementUnselectedListener = function(listener){
+				this.elementUnselectedListener.push(listener);
+			}
+		}
+	};
 	
 }(jQuery));
 
